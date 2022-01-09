597226ca30c5093a451ae6d9d4c3411ada72de0d
Axiks <secure@mail.com>
2022-01-09 23:35:18 +0200

    Add Konatsu Web App. Connect sql server. Add user model

---
+++ KonatsuWebApplication/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using KonatsuWebApplication.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KonatsuWebApplication
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
---
+++ KonatsuWebApplication/Entities/AppUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KonatsuWebApplication.Entities
+{
+    public class AppUser : BaseEntity
+    {
+        public string Username { get; }
+        public int FirstName { get; }
+        public int LastName { get; }
+        public string About { get; set; }
+        // public List<Habit> Habits { get; set; }
+    }
+}
